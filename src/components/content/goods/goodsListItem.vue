c4c79fa45bd41d1d78110252ba4580ec863786a2
Martin730913 <18844216693m@sina.cn>
2019-11-30 14:41:05 +0800

    11.30.2019

--- src/components/content/goods/goodsListItem.vue
+++ src/components/content/goods/goodsListItem.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="goods-item" @click="itemClick">
-		<img :src="showImage" @load="imagedLoad"/>
+		<img v-lazy="showImage" @load="imagedLoad"/>
 		<div class="goods-info">
 			<p>{{goodsItem.title}}</p>
 			<span class="price">{{goodsItem.price}}</span>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import router from "./router/index.js"
 import store from "./store/index"
 import toast from "./components/common/toast/index"
+import fastClick from "fastclick"
+import lazyLoad from "vue-lazyload"
 Vue.config.productionTip = false;
 Vue.prototype.$bus=new Vue();//添加事件总线
 Vue.use(toast);//本质上会执行toast文件里导出对象的install,同时传入vue对象
+Vue.use(lazyLoad,{
+  loading:require("./assets/img/common/placeholder.png")
+});
+fastClick.attach(document.body);
 new Vue({
   render: h => h(App),
   router,
