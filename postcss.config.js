6dcb8c8e52e23fb88216a32dd460f815ece536cb
Martin730913 <18844216693m@sina.cn>
2019-12-01 10:11:39 +0800

    11.30.2019

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
-    // "postcss-px-to-viewport":{
-    //   viewportWidth:375,//视窗的宽度，对应的是设计稿的宽度
-    //   viewportHeight:667,//视窗的高度，对应的是设计稿的高度
-    //   unitPrecision:5,//指定转换后保留的小数位数
-    //   viewportUnit:"vm",//指定要转成的的视窗单位，建议使用vm
-    //   selectorBlackList:["ignore","tar-bar","nav-bar","recommend","tabControl","cartBar","checkButton"],//指定不需要转换的类
-    //   minPixelValue:1,//小于1px的不转换为视窗单位
-    //   mediaQuery:false
-    // }
+    "postcss-px-to-viewport":{
+      viewportWidth:375,//视窗的宽度，对应的是设计稿的宽度
+      viewportHeight:667,//视窗的高度，对应的是设计稿的高度
+      unitPrecision:5,//指定转换后保留的小数位数
+      viewportUnit:"vw",//指定要转成的的视窗单位，建议使用vm
+      selectorBlackList:["ignore","tar-bar"],//指定不需要转换的类
+      minPixelValue:1,//小于1px的不转换为视窗单位
+      mediaQuery:false
+    }
   }
 }
